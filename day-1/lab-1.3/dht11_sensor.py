3f61502057a585c9609a2ab5f1e75f06fe2fe77c
Syukran Hakim <syukran@umpsa.edu.my>
2025-11-23 22:09:08 +0800

    update sensor

--- day-1/lab-1.3/dht11_sensor.py
+++ day-1/lab-1.3/dht11_sensor.py
@@ -3,15 +3,16 @@ import adafruit_dht
 import board
 import time
 
-
-# Sensor configuration
-SENSOR = Adafruit_DHT.DHT11
-PIN = 4  # GPIO4
+dht = adafruit_dht.DHT11(board.D4)  # GPIO4
 
 def main():
     while True:
-        humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-        print(f"Temp: {temperature}°C  Humidity: {humidity}%")
+        try:
+            temp = dht.temperature
+            hum = dht.humidity
+            print(f"Temp={temp}°C Humidity={hum}%")
+        except Exception as e:
+            print("Read error:", e)
         time.sleep(2.0)
 
     return

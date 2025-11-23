cfc589a5843abe48aef2cab0fb37e17fc25de0f7
Syukran Hakim <syukran@umpsa.edu.my>
2025-11-23 22:55:52 +0800

    update gui

--- day-1/lab-1.4/gui.py
+++ day-1/lab-1.4/gui.py
@@ -1,39 +1,64 @@
-from customtkinter import *
+#from customtkinter import *
 import customtkinter as ctk
 
+import RPi.GPIO as GPIO
+import adafruit_dht
+import board
+import time
+
+LED_PIN = 18       # GPIO pin connected to LED
+SWITCH_PIN = 23    # GPIO pin connected to switch
+
+
+# Set data pin to be GPIO5
+dht = adafruit_dht.DHT11(board.D5)
+
+led_state = False
+
+
 def click_handler():
-    print("Clicked")
+    global led_state
+    print("Button clicked")
+    led_state = not led_state
+    GPIO.output(LED_PIN, led_state)
     return
+
+
+def update_label(label, app):
+    try:
+        temp = dht.temperature
+    except Exception as e:
+        pass
+    label.configure(text=f"Temperature is {temp} °C")
+    app.after(2000, update_label, label, app)
 
 
 def main():
 
-    app = CTk()
-    app.geometry("500x400")
-
-    set_appearance_mode("dark")
-
-
-    btn = CTkButton(master=app, text="Click Me", command=click_handler)
-    btn.place(relx=0.5, rely=0.5, anchor="center")
-
-    app.mainloop()
-
-
-    print("Hello Warudo")
-
-
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("blue")
+    # Init GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED_PIN, GPIO.OUT)
+    GPIO.setup(SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # internal pull-up
 
     app = ctk.CTk()
-    app.title("Minimal GUI")
-    app.geometry("600x480")
+    app.geometry("300x240")
+    app.title("IoT GUI")
 
-    btn = ctk.CTkButton(app, text="Click")
-    btn.pack(pady=20)
+    ctk.set_appearance_mode("dark")
+
+    # LED button
+    btn = ctk.CTkButton(master=app, text="Toggle LED", command=click_handler)
+    btn.place(relx=0.5, rely=0.5, anchor="center")
+
+    # Sensor value
+    label = ctk.CTkLabel(app, text='...')
+    label.place(relx=0.5, rely=0.7, anchor="center")
+
+    update_label(label, app)
 
     app.mainloop()
+
+    GPIO.cleanup()
 
     return
 

d6d67fcd4757ba473e8c4b9c7b29059589bb4f9f
Syukran Hakim <syukran@umpsa.edu.my>
2025-11-23 21:03:06 +0800

    restructure

--- day-1/lab-1.4/gui.py
+++ day-1/lab-1.4/gui.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+import customtkinter as ctk
 
 def click_handler():
     print("Clicked")
@@ -19,6 +20,20 @@ def main():
     app.mainloop()
 
 
+    print("Hello Warudo")
+
+
+    ctk.set_appearance_mode("dark")
+    ctk.set_default_color_theme("blue")
+
+    app = ctk.CTk()
+    app.title("Minimal GUI")
+    app.geometry("600x480")
+
+    btn = ctk.CTkButton(app, text="Click")
+    btn.pack(pady=20)
+
+    app.mainloop()
 
     return
 
